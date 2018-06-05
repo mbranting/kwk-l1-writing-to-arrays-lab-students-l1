7a5ea46379c2a37ededae90dad36b2250660888e
mbranting <mckenna.branting@gmail.com>
2018-06-05 22:01:22 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -8,6 +8,7 @@ def change_rainbow_colors
   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
   rainbow_colors['yellow'] = "red"
   rainbow_colors['default'] = "light_red"
+  
   puts rainbow_colors
 end
 
